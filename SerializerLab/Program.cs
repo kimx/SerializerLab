22be5dcf0da445b16c4c59b3c59c3b4c47fc1306
kimx <kimxinfo@gmail.com>
2014-04-09 11:29:01 +0800

    1

---
+++ SerializerLab/Program.cs
@@ -0,0 +1,74 @@
+﻿using Framework.Serialization;
+using NTX.SysCore.Services.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializerLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserMenuInfo info = new UserMenuInfo();
+            info.MVC_ACT = "Kim";
+            info.ORDERNUM = 100;
+            SerializationWriter(info);
+            BinaryFormaterObj(info);
+            BinaryFormaterCommon();
+            Console.ReadLine();
+        }
+
+        private static void SerializationWriter(UserMenuInfo info)
+        {
+            Console.WriteLine("SerializationWriter");
+            SerializationWriter writer = new SerializationWriter(new FileStream(AppDomain.CurrentDomain.BaseDirectory + "info.txt", FileMode.Create));
+            writer.WriteObject(info);
+        }
+
+        private static void BinaryFormaterObj(UserMenuInfo info)
+        {
+            Console.WriteLine("BinaryFormatter-obj");
+            string value = "";
+            using (MemoryStream stream = new MemoryStream())
+            {
+                var binaryWriter = new BinaryFormatter();
+                binaryWriter.Serialize(stream, info);
+                value = Convert.ToBase64String(stream.ToArray());
+                Console.WriteLine(value);
+            }
+
+            using (MemoryStream stream = new MemoryStream(Convert.FromBase64String(value)))
+            {
+                var binaryWriter = new BinaryFormatter();
+                UserMenuInfo info2 = (UserMenuInfo)binaryWriter.Deserialize(stream);
+                Console.WriteLine(info2.MVC_ACT);
+            }
+        }
+
+        private static void BinaryFormaterCommon()
+        {
+            Console.WriteLine("BinaryFormaterCommon");
+            string value = "kim1234567";
+            string valueSerialize = "";
+            using (MemoryStream stream = new MemoryStream())
+            {
+                var binaryWriter = new BinaryFormatter();
+                binaryWriter.Serialize(stream, value);
+                valueSerialize = Convert.ToBase64String(stream.ToArray());
+                Console.WriteLine(value);
+            }
+
+            using (MemoryStream stream = new MemoryStream(Convert.FromBase64String(valueSerialize)))
+            {
+                var binaryWriter = new BinaryFormatter();
+                value = (string)binaryWriter.Deserialize(stream);
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ SerializerLab/UserMenuInfo.cs
@@ -0,0 +1,32 @@
+﻿using SerializerLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NTX.SysCore.Services.Domain
+{
+    [Serializable]
+    /// <summary>
+    /// 讀取使用者已授權的選單
+    /// </summary>
+    public partial class UserMenuInfo : BaseInfo
+    {
+        public string PRG_NO { get; set; }
+        public string PRG_NAME { get; set; }
+        public string PRG_TYPE { get; set; }
+        public string PRG_AREA { get; set; }
+        public string MVC_CTRL { get; set; }
+        public string MVC_ACT { get; set; }
+        public string ENT_POINT { get; set; }
+        public string PARAM_VAL { get; set; }
+        public int ORDERNUM { get; set; }
+        public string UP_PRGNO { get; set; }
+        public int PRG_LEVEL { get; set; }
+        public string ROOT_PATH { get; set; }
+        public string PARENT_PATH { get; set; }
+        public string PRG_PATH { get; set; }
+
+
+    }
+}
